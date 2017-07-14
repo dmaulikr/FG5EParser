9d7aea5da367559f58ff81e6b80771f7dbe0b019
Richard Hodge <richardj@hodgefamily.net>
2017-07-14 12:42:51 -0500

    Initial Project Setup

---
+++ FG5EParser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FG5EParser
+//
+//  Created by Richard Hodge on 7/14/17.
+//  Copyright © 2017 Binary Platypus Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FG5EParser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FG5EParser
+//
+//  Created by Richard Hodge on 7/14/17.
+//  Copyright © 2017 Binary Platypus Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
